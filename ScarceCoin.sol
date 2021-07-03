99494420e67d25ecfa86c1636945878b2dec6f3b
the-scarcecoin <86875195+the-scarcecoin@users.noreply.github.com>
2021-07-03 15:35:36 +0200

    Final Commit

--- ScarceCoin.sol
+++ ScarceCoin.sol
@@ -5,10 +5,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract ScarceCoin is ERC20Capped {
-  address public founderAddress;
-  
   constructor() ERC20("ScarceCoin", "SCARCE") ERC20Capped(21000 * 10 ** 18) {
-      founderAddress = msg.sender;
       ERC20._mint(msg.sender, 21000 * 10 ** 18);
   }
 }
